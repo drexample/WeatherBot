53206a5b3c22064721e0a7b187a815c200ced1d2
drexample <dr.example@mail.ru>
2020-04-12 20:16:33 +0300

    Final

--- weatherman.py
+++ weatherman.py
@@ -24,10 +24,44 @@ class WeatherForm(FlaskForm):
 def index():
     form = WeatherForm()
     if form.validate_on_submit():
-        result = get_weather(form.Place.data)
+        array_returned = get_weather(form.Place.data)
+        result = array_returned[0]
+        if(len(get_weather(form.Place.data)) > 1):
+            result2 = array_returned[1]
     else:
         result = 'Тык.'
-    return render_template('test.html', form=form, result=result)
+        result2 = ''
+    return render_template('test.html', form = form, result = result , result2 = result2)
+
+
+
+def generate_clothes_choice(temperature, weather):
+    current_condition = weather['weather'][0]['main']
+    thing_to_return = []
+    if current_condition == "Thunderstorm":
+        thing_to_return.append("На улице гроза. Советуем не выходить.")
+    if current_condition == "Drizzle":
+        thing_to_return.append("Советуем взять зонт или одежу с капюшоном.")
+    if current_condition == "Rain":
+        thing_to_return.append("Советуем взять зонт или одежу с капюшоном.")
+    if current_condition == "Snow":
+        thing_to_return.append("Советуем взять зонт или одежу с капюшоном.")
+    if weather['clouds']['all'] < 20:
+        thing_to_return.append("Советуем надеть солнечные очки или головной убор с козырьком.")
+    if temperature > 20:
+        thing_to_return.append("Советуем одеться по-легче.")
+    elif temperature > 5:
+        thing_to_return.append("Советуем одеться по-теплее.")
+    elif temperature < 5:
+        thing_to_return.append("Советуем одеться тепло.")    
+    if weather['wind']['speed'] > 30:
+        thing_to_return.append("Сильный ветер. Советуем надеть непродуваемую куртку.")
+    elif weather['wind']['speed'] > 10:
+        thing_to_return.append("Советуем надеть одежду с рукавами.")
+    print(thing_to_return)
+    return " ".join(thing_to_return)
+
+
 def get_weather(weatherplace):
     try:
         res = requests.get("http://api.openweathermap.org/data/2.5/find",
@@ -35,9 +69,7 @@ def get_weather(weatherplace):
         data = res.json()
         cities = ["{} ({})".format(d['name'], d['sys']['country'])
                   for d in data['list']]
-        print("city:", cities)
         city_id = data['list'][0]['id']
-        print('city_id=', city_id)
     except Exception as e:
         print("Exception (find):", e)
         pass
@@ -47,11 +79,8 @@ def get_weather(weatherplace):
                      params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
         data = res.json()
         result = []
-        result = "Погода для населенного пункта " + weatherplace + ": сейчас " + str(data['weather'][0]['description']) + ", температура " + str(data['main']['temp']) + " градусов по Цельсию, скорость ветра: " + str(data['wind']['speed']) + " м/с, максимальная температура на сегодня: " + str(data['main']['temp_max']) + ", минимальная: " + str(data['main']['temp_min'])
-        print(data)
-        print("temp:", data['main']['temp'])
-        print("temp_min:", data['main']['temp_min'])
-        print("temp_max:", data['main']['temp_max'])
+        result.append("Погода для населенного пункта " + weatherplace + ": сейчас " + str(data['weather'][0]['description']) + ", температура " + str(data['main']['temp']) + " градусов по Цельсию, скорость ветра: " + str(data['wind']['speed']) + " м/с, максимальная температура на сегодня: " + str(data['main']['temp_max']) + ", минимальная: " + str(data['main']['temp_min']))
+        result.append(generate_clothes_choice(data['main']['temp'], data))
         return result
     except Exception as e:
         print("Exception (weather):", e)
